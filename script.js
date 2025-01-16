3a7582161cf573948091152df6ce77cb0ea8315b
koffikoffi99 <koffikoffias23@gmail.com>
2025-01-16 20:05:12 +0000

    fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let btn = document.getElementById("clique");
+const btn = document.getElementById("clique");
 btn.addEventListener("click", () => {
     alert("je m'appelle koffi koffi, je viens d'ajouter un produit");
 });
